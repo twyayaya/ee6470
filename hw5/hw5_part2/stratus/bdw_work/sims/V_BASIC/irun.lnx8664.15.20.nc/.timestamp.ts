3774ab8d9aeb48b074b000a571c0048f090a0b97
twyayaya <xxxzzz852@gmail.com>
2021-04-26 13:14:26 +0800

    hw5_part2

---
+++ hw5/hw5_part2/stratus/bdw_work/sims/V_BASIC/irun.lnx8664.15.20.nc/.timestamp.ts
@@ -0,0 +1,4 @@
+1619411527 /home/m109/m109061613/EE6470/hw5_part2/stratus/bdw_work/modules/SobelFilter/BASIC/v_rtl/SobelFilter_ROM_9X32_filter.v
+1619411316 /home/m109/m109061613/EE6470/hw5_part2/stratus/bdw_work/wrappers/SobelFilter_cosim.v
+1619411536 /home/m109/m109061613/EE6470/hw5_part2/stratus/bdw_work/sims/top_V_BASIC.v
+1619411527 /home/m109/m109061613/EE6470/hw5_part2/stratus/bdw_work/modules/SobelFilter/BASIC/SobelFilter_rtl.v
---
+++ hw5/hw5_part2/stratus/bdw_work/sims/V_DPA/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,4 @@
+1619412707 /home/m109/m109061613/EE6470/hw5_part2/stratus/bdw_work/sims/top_V_DPA.v
+1619411316 /home/m109/m109061613/EE6470/hw5_part2/stratus/bdw_work/wrappers/SobelFilter_cosim.v
+1619412698 /home/m109/m109061613/EE6470/hw5_part2/stratus/bdw_work/modules/SobelFilter/DPA/SobelFilter_rtl.v
+1619412698 /home/m109/m109061613/EE6470/hw5_part2/stratus/bdw_work/modules/SobelFilter/DPA/v_rtl/SobelFilter_ROM_9X32_filter.v
